278be7090fadae245f1e3fa9439f914ae725bb7b
Daniil Vysotskiy <dvysotskiy@it-one.ru>
2023-01-14 23:49:23 +0300

    feat: support file forwarding

--- index.js
+++ index.js
@@ -6,42 +6,36 @@ const bot = new TelegramBot(TOKEN, {
   polling: true
 });
 
-const ABOUT_TEXT = `Привет. Я бот, который помогает оформить заявку на перевозку груза. Нужно будет заполнить небольшой опросник. Вот он.
-
-1. Дата готовности
-2. Откуда
-3. Контакты отправителя
-4. Куда
-5. Контакты принимающего
-6. Документы для перевозки
-7. Вес и габариты груза
-8. Дата получения груза
-9. Комментарии
-10. Кто заполнил опросник`
-const QUESTIONS = ['1. Дата готовности груза к получению (у поставщика)', '2. Откуда - Адрес (фактический) забора груза', '3. Контактные данные отдающей стороны (телефон, лицо, название организации)', '4. Куда - Адрес (фактический) доставки груза', '5. Контактные данные принимающей стороны (телефон, лицо, название организации)', '6. Документы для перевозки (если нужна доверенность, то номер счета, если нужны какие-либо другие сопроводительные документы - указать)', '7. Весовые и габаритные характеристики груза', '8. Желаемая дата и время доставки (сроки)', '9. Комментарии (любая доп. информация, например, что нужно сделать, к кому обратиться, какие документы взять/отдать, необходимость заказа пропуска на территорию поставщика/организации)', '10. ФИО заполнявшего опросник'];
-const FINISH_TEXT_1 = 'Молодец! Спасибо за заполненный опросник! Вот что ты указал в нем.';
+const ABOUT_TEXT = `Я бот, который помогает оформить заявку на перевозку груза. Нужно будет заполнить небольшой опросник. Вот он.`
+const TITLES = ['Дата готовности', 'Откуда', 'Контакты отправителя', 'Куда', 'Контакты принимающего', 'Документы для перевозки', 'Вес и габариты груза', 'Дата получения груза', 'Комментарии', 'Кто заполнил опросник'];
+const QUESTIONS = ['Дата готовности груза к получению (у поставщика)', 'Откуда - Адрес (фактический) забора груза', 'Контактные данные отдающей стороны (телефон, лицо, название организации)', 'Куда - Адрес (фактический) доставки груза', 'Контактные данные принимающей стороны (телефон, лицо, название организации)', 'Документы для перевозки (СНАЧАЛА ЗАГРУЗИТЬ ФАЙЛЫ, если есть pdf или фото документов, а ЗАТЕМ УКАЗАТЬ ТЕКСТОМ формат сопроводительных документов)', 'Весовые и габаритные характеристики груза', 'Желаемая дата и время доставки (сроки)', 'Комментарии (любая информация, например, что нужно сделать, к кому обратиться, какие документы взять/отдать, необходимость заказа пропуска на территорию поставщика/организации)', 'ФИО заполнявшего опросник'];
+const FINISH_TEXT_1 = 'Спасибо за заполненный опросник\! Вот что в нем указано.';
 const FINISH_TEST_2 = 'Проверь, все ли указано верно? Если все правильно, жми “Верно” - и я отправлю эти данные в отдел логистики. Если нет - то жми “Нужно поправить”, тогда мы вместе заполним опросник еще раз.';
-const STOP_TEXT = 'Хорошо. Я удалил все записанное. Давай попробуем еще раз, как будешь готов.';
-const COMPLETE_TEXT = 'Сохранено! Давай, пиши еще!';
+const STOP_TEXT = 'Хорошо. Я удалил все записанное. Пиши еще.';
+const COMPLETE_TEXT = 'Сохранено! Давай, пиши еще \/start, когда захочешь\!';
 const SESSION_CHECKLIST_MAP = {};
 
 const resetSession = (id) => {
-  SESSION_CHECKLIST_MAP[id] = { questions: [], answers: [] };
+  SESSION_CHECKLIST_MAP[id] = { questions: [], answers: [], msgIdsWithFiles: [] };
 };
 
 const getNextQuestion = (idx) => {
   return QUESTIONS[idx];
-}
+};
 
 
 const getFormattedAnswers = (id) => {
-  return SESSION_CHECKLIST_MAP[id].answers.map((answer, i) => `${++i}. ${answer}`).join('\n')
+  return SESSION_CHECKLIST_MAP[id].answers.map((answer, i) => `${getNumberForList(i + 1)} ${TITLES[i]}: ${answer}`).join('\n')
+};
+
+const getNumberForList = (number) => {
+  return `${number}.`;
 };
 
 const getFinalReport = (id) => {
   const answers = getFormattedAnswers(id);
   return `${FINISH_TEXT_1} \n\n${answers} \n\n${FINISH_TEST_2}`;
-}
+};
 
 const augmentQuestionsAndAnswers = (msg, idx) => {
   SESSION_CHECKLIST_MAP[msg.chat.id].questions.push(idx + 1);
@@ -50,7 +44,6 @@ const augmentQuestionsAndAnswers = (msg, idx) => {
 
 bot.onText(/\/help/, (msg) => {
   const reply = `/help - показать доступные команды
-/about - показать приветственное сообщение
 /start - начать заполнять опросник
 /stop - прервать заполнение опросника`;
   bot.sendMessage(msg.chat.id, reply);
@@ -58,15 +51,10 @@ bot.onText(/\/help/, (msg) => {
 
 bot.onText(/\/id/, (msg) => {
   bot.sendMessage(msg.chat.id, JSON.stringify(msg.chat));
-})
-
-bot.onText(/\/about/, (msg) => {
-  const reply = ABOUT_TEXT;
-  bot.sendMessage(msg.chat.id, reply);
 });
 
 bot.onText(/\/start/, (msg) => {
-  const reply = ABOUT_TEXT;
+  const reply = `${ABOUT_TEXT} \n\n${TITLES.map((title, i) => `${getNumberForList(++i)} ${title}`).join('\n')}`;
   const opts = {
     reply_markup: {
       inline_keyboard: [
@@ -89,9 +77,11 @@ bot.onText(/\/stop/, (msg) => {
 });
 
 bot.onText(/[^/help|/about|/start|/stop/]+/, (msg) => {
+  const idx = SESSION_CHECKLIST_MAP[msg.chat.id]?.questions.length;
+  if (!idx) return;
+
   let reply = '';
   let opts = {};
-  const idx = SESSION_CHECKLIST_MAP[msg.chat.id].questions.length;
   augmentQuestionsAndAnswers(msg, idx);
 
   if (SESSION_CHECKLIST_MAP[msg.chat.id].questions.length <= QUESTIONS.length) {
@@ -99,6 +89,7 @@ bot.onText(/[^/help|/about|/start|/stop/]+/, (msg) => {
   } else {
     reply = getFinalReport(msg.chat.id);
     opts = {
+      parse_mode: 'Markdown',
       reply_markup: {
         inline_keyboard: [
           [
@@ -132,10 +123,22 @@ bot.on('callback_query', (callbackQuery) => {
   }
 
   if (action === 'valid_checklist') {
-    const answers = getFormattedAnswers(msg.chat.id);
+    const answers = `${getFormattedAnswers(msg.chat.id)} \n\nОтправил: ${msg.chat.username}`;
     reply = COMPLETE_TEXT;
     bot.sendMessage(ADMIN_USER_CHAT_ID, answers);
+    SESSION_CHECKLIST_MAP[msg.chat.id].msgIdsWithFiles.forEach(msgId => {
+      bot.forwardMessage(ADMIN_USER_CHAT_ID, msg.chat.id, msgId);
+    });
   }
 
   bot.sendMessage(msg.chat.id, reply);
 });
+
+bot.on('message', (msg) => {
+  const currentQuestion = SESSION_CHECKLIST_MAP[msg.chat.id]?.questions.length;
+  if ((msg.document || msg.photo) && currentQuestion === 6) {
+    SESSION_CHECKLIST_MAP[msg.chat.id].msgIdsWithFiles.push(msg.message_id);
+  }
+});
+
+bot.on("polling_error", console.log);
